72fbbbe7137302a83e70aea7953de0ed1bfa605f
guille garcia <ggarciacardiel@gmail.com>
2023-08-16 18:32:55 +0200

    adds onSearch function to pass filtered events to slug

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { useState, useEffect, Fragment } from 'react';
+import { useRouter } from 'next/router';
 import { getAllEvents } from '../../services/eventService';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 export default function AllEventsPage() {
   const [allEvents, setAllEvents] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     async function fetchEvents() {
@@ -15,9 +17,14 @@ export default function AllEventsPage() {
     fetchEvents();
   }, []);
 
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <Fragment>
-    <EventsSearch />
+    <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={allEvents} />
     </Fragment>
   )
