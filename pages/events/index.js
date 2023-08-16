654c3b895d1c2e3d58a03e42c3798783a269317f
guille garcia <ggarciacardiel@gmail.com>
2023-08-16 18:07:45 +0200

    adds all events call

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react';
+import { getAllEvents } from '../../services/eventService';
+import EventList from '../../components/events/event-list';
+
 export default function AllEventsPage() {
+  const [allEvents, setAllEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      const events = await getAllEvents();
+      setAllEvents(events);
+    }
+
+    fetchEvents();
+  }, []);
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={allEvents} />
     </div>
   )
 }
