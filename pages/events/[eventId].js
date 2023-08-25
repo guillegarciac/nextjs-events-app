1ccaf196f795ce2aa6132d09d99e51046bff057c
guille garcia <ggarciacardiel@gmail.com>
2023-08-25 17:45:58 +0200

    updates eventId to use staticProps

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,38 +1,18 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { getEventById } from '../../services/eventService';
+import { useRouter } from 'next/router';  // <-- Don't forget to import useRouter
+import { getEventById, getAllEvents } from '../../services/eventService';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 import PuffLoader from "react-spinners/PuffLoader";
 
-export default function EventDetailPage() {
-  const router = useRouter();
-  const { eventId } = router.query;
-  const [event, setEvent] = useState(null);
-  const [loading, setLoading] = useState(true);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
+  const router = useRouter(); 
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (eventId) {
-        getEventById(eventId)
-          .then((eventData) => {
-            setEvent(eventData);
-            setLoading(false);
-          })
-          .catch((error) => {
-            setEvent(null);
-            setLoading(false);
-          });
-      }
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, [eventId]);
-
-  if (loading) {
+  // If the page is in fallback state, show the loader
+  if (router.isFallback) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
         <PuffLoader color={"#03be9f"} size={100} />
@@ -63,3 +43,45 @@ export default function EventDetailPage() {
     </Fragment>
   );
 }
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  
+  const event = await getEventById(eventId);
+
+  if (!event) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 60, 
+  };
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+
+  if (!allEvents) {
+    console.error('No events found');
+    return { paths: [], fallback: true };
+  }
+
+  const paths = allEvents.map(event => {
+    if (!event.id) {
+      console.error('Missing id for event:', event);
+      return null;
+    }
+    return { params: { eventId: event.id.toString() } };
+  }).filter(Boolean);
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+
+export default EventDetailPage;
